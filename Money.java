79fc3c21f86db0d6d4495b985a050441d1fd1080
elieu-web <elyssalieu@gmail.com>
2022-05-10 10:25:18 -0700

    added Jpanel

--- Money.java
+++ Money.java
@@ -1,3 +1,17 @@
-//Money.java
-// testing changes from Elyssa
-// testing changes from Elyssa e
+import java.util.*;
+
+public abstract class Money {
+    private double cost;
+
+    public Money() {
+        cost = 0.0;
+    }
+
+    public Money(double val) {
+        cost = val;
+    }
+
+    public getCost() {
+        return cost;
+    }
+}
