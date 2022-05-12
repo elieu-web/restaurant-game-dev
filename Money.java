cb5473c0a51daecaec0cada631df4456787d6de6
sa6086 <99423200+sa6086@users.noreply.github.com>
2022-05-12 08:56:47 -0700

    Update Money.java

--- Money.java
+++ Money.java
@@ -14,4 +14,14 @@ public abstract class Money {
     public double getCost() {
         return cost;
     }
+    
+    public void roundUp(double amount){
+    int option1 = ((int)Math.random() * 11) + (int)amount;
+    int option2 = ((int)Math.random() * 11) + (int)amount;
+    int option3 = ((int)amount) + 1;
+    System.out.print(option1);
+    System.out.print(option2);
+    System.out.print(option3);
+    System.out.print(option3 + " is the correct answer!");
+}
 }
