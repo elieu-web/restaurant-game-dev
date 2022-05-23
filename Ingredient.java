a49e9aa02dc7977c6fdd38f281a5de6854c33fcc
sa6086 <99423200+sa6086@users.noreply.github.com>
2022-05-23 15:06:44 -0700

    Delete Ingredient.java

--- Ingredient.java
+++
@@ -1,9 +0,0 @@
-public class Ingredient extends Vegetable {
-
-    private Vegetable olive;
-    private Vegetable bellpeppers;
-    private Vegetable onions;
-    private Vegetable tomatoes;
-
-    
-}
