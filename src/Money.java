7d5c47e852706b88d0ed6ed881bbd8f85ca1bd22
ar1518 <99422791+ar1518@users.noreply.github.com>
2022-05-29 16:57:03 -0700

    Update Money.java

--- src/Money.java
+++ src/Money.java
@@ -1,4 +1,5 @@
 package src;
+import java.util.*;
 import java.awt.event.*;
 import java.awt.*;
 import javax.swing.*;
@@ -7,14 +8,16 @@ import javax.swing.plaf.DimensionUIResource;
 import java.awt.GridBagLayout;
 
 
-public class Money extends RestaurantGamePaint{
+public class Money extends RestaurantGamePaint {
     private static final int WIDTH = 500;
-    private static final int HEIGHT = 400;
-    private static JFrame f;
+    private static final int HEIGHT = 600;
+    private static JFrame frame = new JFrame();
     private static JTextField text;
     private static JLabel questionLabel;
     private static JPanel activePanel;
     private double db;
+
+    private static double cost = (int)((Math.random() * 900) + 100) / 100.0; 
 
     public Money (double db2) {
         db = db2;
@@ -24,11 +27,17 @@ public class Money extends RestaurantGamePaint{
     //     db = 0.00;
     // }
 
-    public static JPanel makeOptionPanel(double cost) {
+    public static JPanel makeOptionsPanel() {
+        System.out.println("OPTIONS");
         JPanel p = new JPanel(new GridBagLayout());
         int option1 = (int)cost + (int) (Math.random() * 5);
         int option2 = (int)cost - (int) (Math.random() * 5);
-        int option3 = ((int)cost) + 1; //fix
+        int option3 = ((int)cost) + 1 ; //fix
+
+        questionLabel = new JLabel("Dollar up the price of " + cost);
+        p.add(questionLabel); 
+        p.set(questionLabel).
+
 
         JButton button = new JButton(Integer.toString(option1));
         button.setActionCommand("button1");
@@ -36,26 +45,38 @@ public class Money extends RestaurantGamePaint{
         button2.setActionCommand("button2");
         JButton button3 = new JButton(Integer.toString(option3));
         button3.setActionCommand("button3");
-        button.addActionListener(new Selection());
-        button2.addActionListener(new Selection());
-        button3.addActionListener(new Selection());
+        button.addActionListener(new Money().new Selection());
+        button2.addActionListener(new Money().new Selection());
+        button3.addActionListener(new Money().new Selection());
 
         p.add(button);
         p.add(button2);
         p.add(button3);
         p.setPreferredSize(new Dimension(WIDTH, HEIGHT));
 
+        // activePanel = p;
         return p;
     }
 
-    private static JPanel makeQuestionPanel() {
+    private static JPanel makeIntroPanel() {
         JPanel p = new JPanel(new GridBagLayout());
-        questionLabel = new JLabel("Welcome! Type how many pizzas: ");
-        text = new JTextField("",2);
-        text.addKeyListener(new MyKeyListener());
-        p.add(questionLabel);
-        p.add(text);
+        //questionLabel = new JLabel("Welcome! Type how many pizzas: ");
+        //questionLabel = new JLabel("Welcome! This is your number: " + (double) (Math.random()*100) );
+        //JLabel label = new JLabel("Welcome! This is your number: " + (int) ((Math.random() * 900) + 100) / 100.0);
+        JLabel label = new JLabel("Welcome! This is your number: " + cost);
+        p.add(label);
+
+        JButton button = new JButton("NEXT");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                JPanel p = makeOptionsPanel();
+                setNewPanel(p);
+            }
+        });
+        p.add(button);
         p.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+
         return p;
     }
 
@@ -64,26 +85,20 @@ public class Money extends RestaurantGamePaint{
         JButton button = new JButton("NEXT");
         JButton b2 = new JButton("PREVIOUS");
         JButton b3 = new JButton("HOME");
-        b2.addActionListener(new QuestionPanel());
+        b2.addActionListener(new Money().new QuestionPanel());
         // b3.addActionListener(new HomePanel());
         p.add(b2);
-        //button.addActionListener(new RestaurantGamePaint.PizzaPanel());
+         
         p.add(b3);
         p.add(button);
 
         return p;
     }
 
-    // private static void swapPanels(JPanel newPanel) {
-    //     f.getContentPane().removeAll(); //remove all panels on frame instead of components on panel such as jlabel 
-    //     f.add(newPanel);
-    //     f.validate();
-    //     f.repaint();
-    // }
-
+    //deleted by yash 
     public static Component chooseOptions() {
         JPanel p = new JPanel(); 
-        activePanel = makeQuestionPanel();
+        activePanel = makeIntroPanel();
         GridLayout gl = new GridLayout(3,0);
         p.setLayout(gl);
         p.add(activePanel);
@@ -95,98 +110,128 @@ public class Money extends RestaurantGamePaint{
         return p;
     }
 
-    public static class Selection implements ActionListener {
-        public void actionPerformed(ActionEvent e){
-            // tryNumber += 1;
-            // if (tryNumber == 1 && e.getActionCommand().equals("button3")) {
-            //     points += 100;
-            //     System.out.println("correct!");
-            // } else if (e.getActionCommand().equals("button3")) {
-            //     points += 50;
-            // } else {
-            //     System.out.println ("try again"); 
-            // }
+    //new 
+    // private static JPanel makeMoneyPanel() {
+    //     JPanel p = new JPanel(new GridBagLayout());
+       
+    //     JLabel label = new JLabel("Choose the dollar types to make up " + cost);
+    //     JButton tenDollar = new JButton(resizeImage("images/ten-dollar.png", 100, 100));
 
-            // if (points >= 200) {
+    //     tenDollar.addActionListener(new Money().new QuestionPanel());
+
+
+    //     //text = new JTextField("",2);
+    //     text.addKeyListener(new Money().new MyKeyListener());
+    //     p.add(label);
+    //     //p.add(text);
+    //     p.add(tenDollar); 
+
+    //     p.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    //     return p;
+    // }
+
+
+    // private static void swapPanels(JPanel newPanel) {
+    //     f.getContentPane().removeAll(); //remove all panels on frame instead of components on panel such as jlabel 
+    //     f.add(newPanel);
+    //     f.validate();
+    //     f.repaint();
+    // }
+
+    private static void setNewPanel(JPanel p) {
+        activePanel = p;
+        GridLayout gl = new GridLayout(3,0);
+        frame.setLayout(gl);
+        frame.add(activePanel);
+        frame.setSize(200, 200);
+        frame.setPreferredSize(new DimensionUIResource(WIDTH, HEIGHT));
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.pack();
+    }
+
+    public static void openIntro() {
+        JPanel p = makeIntroPanel();
+        setNewPanel(p);
+    }
+
+
+    public class Selection implements ActionListener {
+        public void actionPerformed(ActionEvent e){
+        
             if (e.getActionCommand().equals("button3")) {
-                ImageIcon winMeme = Pizza.resizeImage("images/WinMeme.png", 200, 200);
+                JPanel winPanel = new JPanel(new GridBagLayout());
+                ImageIcon winMeme = Pizza.resizeImage("images/WinMeme.png", 200, 300);
                 JLabel winLabel = new JLabel(winMeme);
                 // points -= 200;
-                JFrame newFrame = new JFrame("Money Game");
-                activePanel = makeQuestionPanel();
-                GridLayout gl = new GridLayout(1,0);
-                newFrame.setLayout(gl);
+                // JFrame newFrame = new JFrame("Money Game");
+                // activePanel = makeQuestionPanel();
+                // GridLayout gl = new GridLayout(1,0);
+                // newFrame.setLayout(gl);
                 // JPanel winPanel = Pizza.createOverlayPanel(winLabel);
-                // winPanel.add(winLabel);
-                newFrame.setSize(200, 200);
-                newFrame.setPreferredSize(new DimensionUIResource(WIDTH, HEIGHT));
-                newFrame.setVisible(true);
-                newFrame.setLocationRelativeTo(null);
-                newFrame.pack(); 
+                winPanel.add(winLabel);
+                setNewPanel(winPanel);
+                // newFrame.setSize(200, 200);
+                // newFrame.setPreferredSize(new DimensionUIResource(WIDTH, HEIGHT));
+                // newFrame.setVisible(true);
+                // newFrame.setLocationRelativeTo(null);
+                // newFrame.pack(); 
             } else {
                 //add
             }
 
 
-            // JButton button = new JButton(resizeImage("_____ ", 50, 50));
-            // button.setPreferredSize(new DimensionUIResource(50, 50));
-            // JLabel button2 = new JLabel(resizeImage(" ", 50, 50));
-            // button2.setPreferredSize(new DimensionUIResource(50, 50));
-            // JLabel button3 = new JLabel(resizeImage(" ", 50, 50));
-            // button3.setPreferredSize(new DimensionUIResource(50, 50));
-            // //  Setting location
-            // button.setLocation(100, 100);
-            // button2.setLocation(150, 200);
-            // button3.setLocation(200, 300);
-            // p.add(button);
-            // p.add(button2);
-            // p.add(button3);
-            // p.revalidate();
-            // p.repaint();
+          
             
 
 
         }
     }
 
-    public static class MyKeyListener implements KeyListener {
-        public void keyPressed(KeyEvent e) {
-            if (e.getKeyCode()== KeyEvent.VK_ENTER) {
-                String s = text.getText();
-                try {
-                    Double i = Double.parseDouble(s);
-                    questionLabel = new JLabel("Dollar up the price of " + Calculator.getPrice(i),SwingConstants.CENTER);
-                    JPanel questionPanel = new JPanel();
-                    questionPanel.add(questionLabel);
-                    activePanel = makeOptionPanel(i * 15.25);
+    // public class MyKeyListener implements KeyListener {
+    //     public void keyPressed(KeyEvent e) {
+    //         if (e.getKeyCode()== KeyEvent.VK_ENTER) {
+    //             String s = text.getText();
+    //             try {
+    //                 Double i = Double.parseDouble(s);
+    //                 //questionLabel = new JLabel("Dollar up the price of " + Calculator.getPrice(i),SwingConstants.CENTER);
+    //                 questionLabel = new JLabel("Dollar up the price of " + cost);
 
-                    f.getContentPane().removeAll();
-                    f.add(questionPanel);
-                    f.add(activePanel);
-                    f.add(makeButtons());
-                    f.validate();
-                    f.repaint();
-                } catch (NumberFormatException n) {
-                    questionLabel = new JLabel("Sorry, that's not a number! Please go back and try again.", SwingConstants.CENTER);
-                    activePanel.revalidate();
-                    activePanel.repaint();
-                    f.repaint();
-                }
-            }
-        }
+    //                 JPanel questionPanel = new JPanel();
+    //                 questionPanel.add(questionLabel);
+    //                 activePanel = Money.makeOptionPanel();
 
-        public void keyTyped(KeyEvent e) {
-        }
+    //                 f.getContentPane().removeAll();
+    //                 f.add(questionPanel);
+    //                 f.add(activePanel);
+    //                 f.add(makeButtons());
+    //                 f.validate();
+    //                 f.repaint();
+    //             } catch (NumberFormatException n) {
+    //                 questionLabel = new JLabel("Sorry, that's not a number! Please go back and try again.", SwingConstants.CENTER);
+    //                 activePanel.revalidate();
+    //                 activePanel.repaint();
+    //                 f.repaint();
+    //             }
+    //         }
+    //     }
 
-        public void keyReleased(KeyEvent e) {
-        }
-    }
+        // public void keyTyped(KeyEvent e) {
+        // }
 
-    public static class QuestionPanel implements ActionListener {
+        // public void keyReleased(KeyEvent e) {
+        // }
+    // }
+
+    public class QuestionPanel implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             JOptionPane.showMessageDialog(null, "No questions found to go back to", "error", JOptionPane.ERROR_MESSAGE);
         }
     }
+
+  
+
+   
 
     // public class ClickDollar implements ActionListener {
     //     public void actionPerformed(ActionEvent e){
@@ -215,32 +260,5 @@ public class Money extends RestaurantGamePaint{
     //     }
     // }
 
-    // public static void payMoney() {
-
-    //     JLabel picLabel = new JLabel(new ImageIcon("__"));
-
-    //     JButton tenDollar = new JButton(resizeImage("images/ten-dollar.png", 100, 100));
-    //     button.setPreferredSize(new DimensionUIResource(100, 100));
-
-    //     JButton fiveDollar = new JButton(resizeImage("images/five-dollar.png", 100, 100));
-    //     button1.setPreferredSize(new DimensionUIResource(100, 100));
-
-    //     JButton oneDollar = new JButton(resizeImage("images/one-dollar.png", 100, 100));
-    //     button2.setPreferredSize(new DimensionUIResource(100, 100));
-
-    //     JButton quarter = new JButton(resizeImage("images/quarter.jpg", 100, 100));
-    //     button3.setPreferredSize(new DimensionUIResource(100, 100));
-
-    //     Jbutton dime = new JButton(resizeImage("images/dime.jpg," 100, 100));
-    //     button4.setPreferredSize(new DimensionUIResource(100, 100)); 
-
-    //     p.add(button);
-    //     p.add(button1);
-    //     p.add(button2);
-    //     p.add(button3);
-    //     p.add(button4);
-
-    // }
-
-    //add next button 
+    
 }
